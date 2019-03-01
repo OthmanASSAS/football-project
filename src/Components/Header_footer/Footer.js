1e999e5ad7ca5151dc8acd8818256033138e04d8
Othman ASSAS <o.assas@gop.link>
2019-03-01 17:08:53 +0100

    footer

--- src/Components/Header_footer/Footer.js
+++ src/Components/Header_footer/Footer.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import { CityLogo } from "../ui/Icons";
 
 const Footer = () => {
-    return (
-        <div>
-            FOOTER
-        </div>
-    );
+  return (
+    <footer className="bck_blue">
+      <div className="footer_logo">
+        <CityLogo 
+            width="70px"
+            height="70px"
+            link={true}
+            linkTo="/"
+        />
+      </div>
+      <div className="footer_discl">Manchester city 2018. All rights reserved.</div>
+    </footer>
+  );
 };
 
 export default Footer;
